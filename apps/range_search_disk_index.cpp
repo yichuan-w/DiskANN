8ea97773abcd23c93a09a4c19ee43161e15d2c9c
Andy Lee <andylizf@outlook.com>
2025-04-01 16:38:02 +0000

    revert some useless files

--- apps/range_search_disk_index.cpp
+++ apps/range_search_disk_index.cpp
@@ -21,11 +21,6 @@
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <unistd.h>
-#endif
-
-#ifdef __APPLE__
-#include "apple_aligned_file_reader.h"
-#elif __linux__
 #include "linux_aligned_file_reader.h"
 #else
 #ifdef USE_BING_INFRA
@@ -98,8 +93,6 @@ int search_disk_index(diskann::Metric &metric, const std::string &index_path_pre
 #else
     reader.reset(new diskann::BingAlignedFileReader());
 #endif
-#elif __APPLE__
-    reader.reset(new AppleAlignedFileReader());
 #else
     reader.reset(new LinuxAlignedFileReader());
 #endif
@@ -127,7 +120,7 @@ int search_disk_index(diskann::Metric &metric, const std::string &index_path_pre
     omp_set_num_threads(num_threads);
 
     uint64_t warmup_L = 20;
-    size_t warmup_num = 0, warmup_dim = 0, warmup_aligned_dim = 0;
+    uint64_t warmup_num = 0, warmup_dim = 0, warmup_aligned_dim = 0;
     T *warmup = nullptr;
 
     if (WARMUP)
--- apps/search_disk_index.cpp
+++ apps/search_disk_index.cpp
@@ -18,11 +18,7 @@
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <unistd.h>
-#ifdef __APPLE__
-#include "apple_aligned_file_reader.h"
-#else
 #include "linux_aligned_file_reader.h"
-#endif
 #else
 #ifdef USE_BING_INFRA
 #include "bing_aligned_file_reader.h"
@@ -109,8 +105,6 @@ int search_disk_index(diskann::Metric &metric, const std::string &index_path_pre
 #else
     reader.reset(new diskann::BingAlignedFileReader());
 #endif
-#elif __APPLE__
-    reader.reset(new AppleAlignedFileReader());
 #else
     reader.reset(new LinuxAlignedFileReader());
 #endif
@@ -138,7 +132,7 @@ int search_disk_index(diskann::Metric &metric, const std::string &index_path_pre
     omp_set_num_threads(num_threads);
 
     uint64_t warmup_L = 20;
-    size_t warmup_num = 0, warmup_dim = 0, warmup_aligned_dim = 0;
+    uint64_t warmup_num = 0, warmup_dim = 0, warmup_aligned_dim = 0;
     T *warmup = nullptr;
 
     if (WARMUP)
